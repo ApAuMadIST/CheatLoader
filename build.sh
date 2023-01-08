a873fd26be1021e41aa7337cbeabe7c0ba98a098
ApAuMadIST <earthprulore@gmail.com>
2023-01-09 01:36:09 +0700

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-[ -f build/minecraft.jar ] && rm build/minecraft.jar
+[ -f build/HaxLoader.zip ] && rm build/HaxLoader.zip
 
-java -jar RetroMCP-Java-all.jar build -fullbuild
+java -jar RetroMCP-Java-all.jar build
+7z d build/minecraft.zip yz.class
+
+(
+cd loader
+7z a ../build/minecraft.zip mod_HaxLoader.class
+)
+
+mv build/minecraft.zip build/HaxLoader.zip
