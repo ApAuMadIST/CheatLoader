fa96de79136785dddc829a562fa8292ad891a840
ApAuMadIST <earthprulore@gmail.com>
2023-01-09 02:26:00 +0700

    fix mcp_setup

--- mcp_setup.sh
+++ mcp_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mv src srcP
-java -jar RetroMCP-Java-all.jar setup
+java -jar RetroMCP-Java-all.jar setup b1.7.3
 java -jar RetroMCP-Java-all.jar decompile
-rm src
+rm -rf src
 mv srcP src
